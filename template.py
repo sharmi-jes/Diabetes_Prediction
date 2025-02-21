8858d8e546cf80306f0394e0ec6e36623c19b321
sharmi-jes <anyumsharmila@gmail.com>
2025-02-21 22:47:14 +0530

    added trainingpipeline

--- template.py
+++ template.py
@@ -13,15 +13,18 @@ logging.info("Starting file creation process...")
 # List of files to create
 list_of_files = [
     '.github/workflows/.gitkeep',
-    f'src/{project_name}/components/__init__.py',
-    f'src/{project_name}/constants/__init__.py',
-    f'src/{project_name}/entity/__init__.py',
-    f'src/{project_name}/pipeline/__init__.py',
-    f'src/{project_name}/utils/__init__.py',
+    f'Diabetes/{project_name}/components/__init__.py',
+    f'Diabetes/{project_name}/constants/__init__.py',
+    f'Diabetes/{project_name}/entity/__init__.py',
+    f'Diabetes/{project_name}/pipeline/__init__.py',
+    f'Diabetes/{project_name}/utils/__init__.py',
+    f'Diabetes/logging.py',
+    f'Diabetes/exception.py',
     'setup.py',
     'templates/index.html',
     'app.py',
     'main.py',
+    'push.py'
 ]
 
 # Creating directories and files
@@ -30,16 +33,17 @@ for filename in list_of_files:
     file_dir, file_name = os.path.split(filepath)
 
     # Create directory if it does not exist
-    if file_dir and not os.path.exists(file_dir):
+    if file_dir:
         os.makedirs(file_dir, exist_ok=True)
         logging.info(f"Created directory: {file_dir}")
 
     # Create file if it does not exist or is empty
-    if not os.path.exists(filepath) or os.path.getsize(filepath) == 0:
+    if not filepath.exists() or filepath.stat().st_size == 0:
         with open(filepath, "w") as f:
             pass
         logging.info(f"Created file: {filepath}")
     else:
         logging.info(f"File already exists: {filepath}")
+
 
 
